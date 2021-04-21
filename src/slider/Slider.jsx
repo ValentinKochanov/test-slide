b4ff733a1a0bfdc91b25d402751617263a9afabc
ValentinKochanov <valentinkocanov299@gmail.com>
2021-04-21 20:04:16 +0400

    add animation

--- src/slider/Slider.jsx
+++ src/slider/Slider.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import styles from './slide.module.css';
 import Paginator from './Paginator';
 
@@ -10,7 +11,14 @@ export default function Slider(props){
     <div className={styles.pagination}><Paginator state={props.count}/></div>
     <div className={styles.slider}></div>
     <div className={styles.slider}></div>
-    <div id="myRef1" className={styles.slider}></div>
+    <div id="myRef1" className={classNames(styles.slider, styles.slide1)}>
+      <h1 className={styles.header1}>Всегда ли цели терапии СД2 на поверхности?</h1>
+      {/* <p className={styles.HbA1}>Цель по HbA1c</p>
+      <p className={styles.gipoglicemia}>Гипогликемия</p>
+      <p className={styles.oslozhnenia}>Осложнения СД</p>
+      <p className={styles.risky}>СС риски</p>
+      <p className={styles.listaite} align="center">Листайте вниз</p> */}
+    </div>
     <div className={styles.slider}></div>
     <div className={styles.slider}></div>
     <div id="myRef2" className={styles.slider}></div>
