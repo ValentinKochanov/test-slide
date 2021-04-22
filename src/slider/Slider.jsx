1e17cedd961d3eceee7350fc079c659909324bab
ValentinKochanov <valentinkocanov299@gmail.com>
2021-04-22 10:16:01 +0400

    add page-proofs

--- src/slider/Slider.jsx
+++ src/slider/Slider.jsx
@@ -13,15 +13,17 @@ export default function Slider(props){
     <div className={styles.slider}></div>
     <div id="myRef1" className={classNames(styles.slider, styles.slide1)}>
       <h1 className={styles.header1}>Всегда ли цели терапии СД2 на поверхности?</h1>
-      {/* <p className={styles.HbA1}>Цель по HbA1c</p>
+      <p className={styles.HbA1}>Цель по HbA1c</p>
       <p className={styles.gipoglicemia}>Гипогликемия</p>
-      <p className={styles.oslozhnenia}>Осложнения СД</p>
       <p className={styles.risky}>СС риски</p>
-      <p className={styles.listaite} align="center">Листайте вниз</p> */}
+      <p className={styles.oslozhnenia}>Осложнения СД</p>
+      <p className={styles.listaite} align="center">Листайте вниз</p>
     </div>
     <div className={styles.slider}></div>
     <div className={styles.slider}></div>
-    <div id="myRef2" className={styles.slider}></div>
+    <div id="myRef2" className={styles.slider}>
+      <h1 className={styles.header2}>Основа терапии - патогенез СД</h1>
+    </div>
     <div id="myRef5" className={styles.slider}></div>
     <div id="myRef4" className={styles.slider}></div>
     <div id="myRef3" className={styles.slider}></div> 
